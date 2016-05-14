526ac626684cfa10f46339a5364a2bd7359d29b0
dreamingblackcat <dreamingblackcat10@gmail.com>
2016-05-14 15:37:25 +0630

    Add Gridlist from MUI

--- components/githuber_list.js
+++ components/githuber_list.js
@@ -1,6 +1,11 @@
 import React, { Component  } from 'react';
 import { fetchGithubers } from '../actions';
 
+import {GridList, GridTile} from 'material-ui/GridList';
+import IconButton from 'material-ui/IconButton';
+import StarBorder from 'material-ui/svg-icons/toggle/star-border';
+
+
 export default class GithuberList extends Component {
 
   componentDidMount() {
@@ -10,12 +15,45 @@ export default class GithuberList extends Component {
 
   render() {
     const { githubers } = this.props;
+
+    const styles = {
+      root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+      },
+      gridList: {
+        width: '600px',
+        overflowY: 'auto',
+        marginBottom: 24,
+      },
+    };
+
     return (
-      <ul>
-        {
-          githubers.map( (githuber, index) => {return <li key={index}> {githuber.login } </li>})
-        }
-      </ul>
+      <div style={styles.root}>
+        <GridList
+          cols={4}
+          cellHeight={200}
+          padding={1}
+          style={styles.gridList}
+        >
+        {githubers.map( (githuber, index) => (
+          <GridTile
+            key={index}
+            title={githuber.login}
+            actionIcon={<IconButton><StarBorder color="white" /></IconButton>}
+            actionPosition="left"
+            titlePosition="top"
+            titleBackground="linear-gradient(to bottom, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.3) 70%,rgba(0,0,0,0) 100%)"
+            cols={1}
+          >
+            <img src={githuber.avatar_url} />
+          </GridTile>
+        ))}
+        </GridList>
+      </div>
     );
   }
 };
+
+
--- containers/app.js
+++ containers/app.js
@@ -2,6 +2,8 @@ import React, { Component  } from 'react';
 import {connect} from 'react-redux';
 import * as actions from '../actions';
 import GithuberList from '../components/githuber_list';
+import baseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
 const mapStateToProps = (state) => {
   return {githubers: state.githubers};
@@ -9,9 +11,21 @@ const mapStateToProps = (state) => {
 
 
 class App extends Component {
+
+  static get childContextTypes() { 
+    return { muiTheme: React.PropTypes.object.isRequired }
+  };
+
+  getChildContext() {
+    return {
+      muiTheme: getMuiTheme(baseTheme)
+    };
+  }
+
   render() {
     return <GithuberList githubers={this.props.githubers} dispatch={this.props.dispatch}/>;
   }
 };
+
 
 export default connect(mapStateToProps)(App)
